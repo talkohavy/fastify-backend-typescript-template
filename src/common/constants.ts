5918da6cd99d7b37d5f4d2d67850e7941a2d5a4e
talkohavy <51020442+talkohavy@users.noreply.github.com>
2025-12-06 16:38:39 +0200

    added routes to API_URLS

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,14 @@
 export const API_URLS = {
   healthCheck: '/api/health-check',
+  // users
+  users: '/api/users',
+  userById: '/api/users/:userId',
+  getProfile: '/api/users/get-profile',
+  getUserByEmail: '/api/users/get-by-email',
+  // validation examples
+  validateBodyByJson: '/api/validation/validate-body-by-json',
+  validateQueryParamsByJson: '/api/validation/validate-query-params-by-json',
+  validateParamsByJson: '/api/validation/validate-params-by-json',
+  validateHeadersByJson: '/api/validation/validate-headers-by-json',
+  validatePreAddedSchema: '/api/validation/validate-pre-added-schema',
 };
