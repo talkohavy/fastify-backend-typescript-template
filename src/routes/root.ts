9178b2a006e3ef7ed586b9cc76b1705f21fcaff9
talkohavy <51020442+talkohavy@users.noreply.github.com>
2025-12-11 08:20:47 +0200

    deleted routes folder

--- src/routes/root.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyInstance, FastifyPluginOptions } from 'fastify';
-
-export default async function root(fastify: FastifyInstance, _opts: FastifyPluginOptions): Promise<void> {
-  fastify.get('/', async (_request, _reply) => {
-    return { root: true };
-  });
-}
